b4bc96a86125f103cef21d67d6e89e4197a187b4
Dreaded Wire <129433129+dreaded-wire@users.noreply.github.com>
2024-10-14 20:00:01 -0500

    Update main.py
    Significant performance changes in URL and user input handling

--- main.py
+++ main.py
@@ -1,13 +1,90 @@
 from person_module import Person
 from address_module import StreetAddress
-from phone_module import PhoneNumber  # Import the PhoneNumber module
+from phone_module import PhoneNumber
 from username_module import Username
 from email_module import Email
 from clear_module import clear_screen
 from ip_module import IP
 from banner import banner
 
-# Example usage:
+def display_urls(urls): # URL output
+    for url in urls:
+        print(url)
+    input("\nPress enter to continue: ")
+
+def handle_choice(option): # Sets URL generation methods
+    if option == "1":
+        person = Person()
+        person.update_info()
+        person.display_info()
+        urls = [
+            person.generate_thatsthem_url(),
+            person.generate_truepeoplesearch_url(),
+            person.generate_411_url(),
+            person.generate_usphonebook_url(),
+            person.generate_familytreenow_url(),
+            person.generate_zabasearch_url()
+        ]
+    elif option == "2":
+        street_address = StreetAddress()
+        street_address.update_info()
+        street_address.display_info()
+        urls = [
+            street_address.generate_truepeoplesearch_url(),
+            street_address.generate_thatsthem_url(),
+            street_address.generate_zillow_url()
+        ]
+    elif option == "3":
+        phone_number = PhoneNumber()
+        phone_number.update_info()
+        phone_number.display_info()
+        urls = [
+            phone_number.generate_truepeoplesearch_url(),
+            phone_number.generate_411_url(),
+            phone_number.generate_thatsthem_url(),
+            phone_number.generate_usphonebook_url(),
+            phone_number.generate_npnr_url(),
+            phone_number.generate_whocalld_url(),
+            phone_number.generate_reversephonecheck_url1(),
+            phone_number.generate_reversephonecheck_url2(),
+            phone_number.generate_thisnumber_url()
+        ]
+    elif option == "4":
+        username = Username()
+        username.update_info()
+        username.display_info()
+        urls = [
+            username.generate_idcrawl_url(),
+            username.generate_uvrX_url()
+        ]
+    elif option == "5":
+        email = Email()
+        email.update_info()
+        email.display_info()
+        urls = [
+            email.generate_thatsthem_url(),
+            email.generate_truepeoplesearch_url(),
+            email.generate_hunter_io_url()
+        ]
+    elif option == "6":
+        ip = IP()
+        ip.update()
+        ip.display()
+        urls = [
+            ip.generate_whatismyipaddress(),
+            ip.generate_mxtoolbox(),
+            ip.generate_thatsthem(),
+            ip.generate_keycdn(),
+            ip.generate_geolocation()
+        ]
+    else:
+        clear_screen()
+        print("Invalid choice.")
+        return
+
+    display_urls(urls)
+
+# Main loop
 while True:
     clear_screen()
     banner()
@@ -20,144 +97,7 @@ while True:
     print("0. Exit\n")
 
     choice = input("Enter numeric option: ")
-
     if choice == "0":
         exit()
 
-    if choice == "1":
-        # Create a Person object
-        person = Person()
-
-        # Update information
-        person.update_info()
-
-        # Display updated information
-        person.display_info()
-
-        # Generate and display persons URLs
-        thatsthem = person.generate_thatsthem_url()
-        truepeoplesearch = person.generate_truepeoplesearch_url()
-        four = person.generate_411_url()
-        usphone = person.generate_usphonebook_url()
-        famtree = person.generate_familytreenow_url()
-        zaba = person.generate_zabasearch_url()
-        print(zaba)
-        print(famtree)
-        print(usphone)
-        print(thatsthem)
-        print(truepeoplesearch)
-        print(four)
-        input("\nPress enter to continue: ")
-    elif choice == "2":
-        # Create a StreetAddress object
-        street_address = StreetAddress()
-
-        # Update information
-        street_address.update_info()
-
-        # Display updated information
-        street_address.display_info()
-
-        # Generate and display address URLs
-        truepeoplesearch = street_address.generate_truepeoplesearch_url()
-        thatsthem = street_address.generate_thatsthem_url()
-        zillow = street_address.generate_zillow_url()
-        print(zillow)
-        print(truepeoplesearch)
-        print(thatsthem)
-        input("\nPress enter to continue: ")
-    elif choice == "3":
-        # Create a PhoneNumber object
-        phone_number = PhoneNumber()
-
-        # Update information
-        phone_number.update_info()
-
-        # Display updated information
-        phone_number.display_info()
-
-        # Generate and display phone URLs
-        truepeoplesearch = phone_number.generate_truepeoplesearch_url()
-        four = phone_number.generate_411_url()
-        thatsthem = phone_number.generate_thatsthem_url()
-        usphonebook = phone_number.generate_usphonebook_url()
-        npnr = phone_number.generate_npnr_url()
-        whocalld = phone_number.generate_whocalld_url()
-        reversephonecheck_url1 = phone_number.generate_reversephonecheck_url1()
-        reversephonecheck_url2 = phone_number.generate_reversephonecheck_url2()
-        thisnumber = phone_number.generate_thisnumber_url()
-
-        print(truepeoplesearch)
-        print(four)
-        print(thatsthem)
-        print(usphonebook)
-        print(npnr)
-        print(whocalld)
-        print(reversephonecheck_url1)
-        print(reversephonecheck_url2)
-        print(thisnumber)
-        input("\nPress enter to continue: ")
-
-    elif choice == "4":
-        # Create Username object
-        username = Username()
-
-        # Update information
-        username.update_info()
-
-        # Display updated informaion
-        username.display_info()
-
-        # Generate and display username URLs
-        idcrawl = username.generate_idcrawl_url()
-        uvrX = username.generate_uvrX_url()
-
-        print(idcrawl)
-        print(uvrX)
-        input("\nPress enter to continue: ")
-
-    elif choice == "5":
-       # Create Email object
-       email = Email()
-
-       # Update information
-       email.update_info()
-
-       # Display updated information
-       email.display_info()
-
-       # Generate and display email URLs
-       thatsthem = email.generate_thatsthem_url()
-       truepeoplesearch = email.generate_truepeoplesearch_url()
-       hunter = email.generate_hunter_io_url()
-
-       print(thatsthem)
-       print(truepeoplesearch)
-       print(hunter)
-       input("\nPress enter to continue: ")
-    elif choice == "6":
-        # Create IP object
-        ip = IP()
-
-        # Update information
-        ip.update()
-
-        # Display updated information
-        ip.display()
-
-        # Generate and display ip URLs
-        whatis = ip.generate_whatismyipaddress()
-        mx = ip.generate_mxtoolbox()
-        thatsthem = ip.generate_thatsthem()
-        keycdn = ip.generate_keycdn()
-        geo = ip.generate_geolocation()
-
-        print(whatis)
-        print(mx)
-        print(thatsthem)
-        print(keycdn)
-        print(geo)
-        input("\nPress enter to continue: ")
-    else: # Error handler
-        clear_screen()
-        print("Invalid choice.")
+    handle_choice(choice)
